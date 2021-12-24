56d18519270cdaf42865f110942fd032b2f0bd43
Dione Maciel <dionerm86@gmail.com>
2021-12-23 21:31:37 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_FluentNHibernate/Controllers/AlunosController.cs
@@ -0,0 +1,136 @@
+﻿using CRUD_FluentNHibernate.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using NHibernate.Linq;
+using System.Linq;
+using System.Web;
+using ISession = NHibernate.ISession;
+
+namespace CRUD_FluentNHibernate.Controllers
+{
+    public class AlunosController : Controller
+    {
+        // GET: AlunosController
+        public ActionResult Index()
+        {
+            using (NHibernate.ISession session = NHibernateHelper.OpenSession())
+            {
+                var alunos = session.Query<Aluno>().ToList();
+                return View(alunos);
+            }
+
+        }
+
+        // GET: AlunosController/Details/5
+        public ActionResult Details(int id)
+        {
+            using (NHibernate.ISession session = NHibernateHelper.OpenSession())
+            {
+                var aluno = session.Get<Aluno>(id);
+                return View(aluno);
+            }
+        }
+
+        // GET: AlunosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AlunosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Aluno aluno)
+        {
+            try
+            {
+                using (ISession session = NHibernateHelper.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(aluno);
+                        transaction.Commit();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AlunosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var aluno = session.Get<Aluno>(id);
+                return View(aluno);
+            }
+        }
+
+        // POST: AlunosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Aluno aluno)
+        {
+            try
+            {
+                using (ISession session = NHibernateHelper.OpenSession())
+                {
+                    var alunoAlterado = session.Get<Aluno>(id);
+                    alunoAlterado.Sexo = aluno.Sexo;
+                    alunoAlterado.Curso = aluno.Curso;
+                    alunoAlterado.Email = aluno.Email;
+                    alunoAlterado.Nome = aluno.Nome;
+
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(alunoAlterado);
+                        transaction.Commit();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AlunosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var aluno = session.Get<Aluno>(id);
+                return View(aluno);
+            }
+        }
+
+        // POST: AlunosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Aluno aluno)
+        {
+            try
+            {
+                using (ISession session = NHibernateHelper.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Delete(aluno);
+                        transaction.Commit();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUD_FluentNHibernate/Models/AlunoMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_FluentNHibernate.Models
+{
+    public class AlunoMap : ClassMap<Aluno>
+    {
+        public AlunoMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Nome);
+            Map(x => x.Email);
+            Map(x => x.Curso);
+            Map(x => x.Sexo);
+            Table("ALUNOS");
+        }
+    }
+}
